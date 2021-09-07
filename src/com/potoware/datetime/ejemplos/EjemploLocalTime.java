c0d2848f0fbc1136984315e85a055fa06cfa118f
Alejandro Potosi <alejandropotosi@outlook.com>
2021-09-07 13:22:18 -0500

    Formatear Hora con DateTimeFormat

--- src/com/potoware/datetime/ejemplos/EjemploLocalTime.java
+++ src/com/potoware/datetime/ejemplos/EjemploLocalTime.java
@@ -2,6 +2,7 @@ package com.potoware.datetime.ejemplos;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 public class EjemploLocalTime {
@@ -21,7 +22,20 @@ public class EjemploLocalTime {
         LocalTime sieteConTreinta =LocalTime.from(seisConTreinta).plusHours(1);
         System.out.println("sieteConTreinta = " + sieteConTreinta);
 
-        boolean esAnterior = LocalTime.of(6,30).isBefore( LocalTime.of(6,20));
+        boolean esAnterior = LocalTime.of(6,30,20).isBefore( LocalTime.of(6,20,20));
         System.out.println("esAnterior = " + esAnterior);
+
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("hh:mm:ss a");
+        String ahoraFormateado = seisConTreinta.format(df);
+        System.out.println("ahoraFormateado = " + ahoraFormateado);
+        
+        ahoraFormateado = df.format(ahora);
+
+        System.out.println("ahoraFormateado = " + ahoraFormateado);
+        
+        LocalTime max = LocalTime.MAX;
+        LocalTime min = LocalTime.MIN;
+        System.out.println("min = " + min);
+        System.out.println("max = " + max);
     }
 }
