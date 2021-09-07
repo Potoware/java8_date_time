424342645e5b498a866de189134f87da25f48e7f
Alejandro Potosi <alejandropotosi@outlook.com>
2021-09-07 13:47:37 -0500

    Usando ZoneDateTime y mas formas

--- src/com/potoware/datetime/ejemplos/EjemploZoneDateTime.java
+++ src/com/potoware/datetime/ejemplos/EjemploZoneDateTime.java
@@ -4,18 +4,27 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class EjemploZoneDateTime {
     public static void main(String[] args) {
-        LocalDateTime fechaLocal = LocalDateTime.now();
+
+
+        LocalDateTime fechaLocal = LocalDateTime.parse("2021-11-20T03:12");
         ZoneId newyork = ZoneId.of("America/New_York");
         
         ZonedDateTime zonaNy = ZonedDateTime.of(fechaLocal,newyork);
         System.out.println("zonaNy = " + zonaNy);
 
         ZoneId madrid = ZoneId.of("Europe/Madrid");
-        ZonedDateTime zonaMadrid =zonaNy.withZoneSameInstant(madrid);
+        ZonedDateTime zonaMadrid =zonaNy.withZoneSameInstant(madrid).plusHours(8);
         System.out.println("zonaMadrid = " + zonaMadrid);
-        
+
+        DateTimeFormatter f = DateTimeFormatter.ofPattern("HH:mm, dd MMM yyyy");
+        System.out.println("Detalles del viaje a españa = ");
+        System.out.println("Partida Nueva York: "+ f.format(zonaNy) );
+        System.out.println("Llegada España: "+ f.format(zonaMadrid) );
+
     }
 }
